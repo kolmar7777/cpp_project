56b4d7f6dee60c3723cedb239c9dae9de94a0bcc
Nikolay <74612113+kolmar7777@users.noreply.github.com>
2022-11-23 11:36:57 +0300

    fix uninitialized variable
    Uninitialized variables: sum_odd, sum_even.

--- sumOfOddAndEvenNumber.cpp
+++ sumOfOddAndEvenNumber.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main()
 {
     long long n;
-    int sum_odd , sum_even ;
+    int sum_odd = 0 , sum_even = 0;
     cin >> n; cin.ignore();
     for (int i = 1; i <= n; i+=2)
     {
